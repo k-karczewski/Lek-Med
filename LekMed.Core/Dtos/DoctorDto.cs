0792008a58490ceda31567b99141493410ba91af
Konrad Karczewski <kkarczewski94@gmail.com>
2021-01-30 22:37:25 +0100

    First version of Dtos

---
+++ LekMed.Core/Dtos/DoctorDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace LekMed.Core
+{
+    public class DoctorDto
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public int WorkYears { get; set; }
+        public bool IsAbleToMakePrescription { get; set; }
+
+        public List<PrescriptionDto> Prescriptions { get; set; }
+    }
+}
---
+++ LekMed.Core/Dtos/MedicineDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LekMed.Core
+{
+    public class MedicineDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CompanyName { get; set; }
+        public string ActiveSubstance { get; set; }
+        public decimal Weight { get; set; }
+        public decimal Price { get; set; }
+        public int Amount { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+        public PrescriptionDto Prescription { get; set; }
+    }
+}
