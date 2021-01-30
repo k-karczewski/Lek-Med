0290c947d2e797b0f332cea69abee9b4b687931d
Konrad Karczewski <kkarczewski94@gmail.com>
2021-01-30 19:45:06 +0100

    Migrations

---
+++ LekMed.Database/Migrations/20210130175443_InitialDatabase.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LekMed.Database.Migrations
+{
+    public partial class InitialDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DoctorEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<int>(nullable: false),
+                    LastName = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    WorkYears = table.Column<int>(nullable: false),
+                    IsAbleToMakePrescription = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DoctorEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prescriptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    DoctorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prescriptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Prescriptions_DoctorEntity_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "DoctorEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MedicineEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    CompanyName = table.Column<string>(nullable: true),
+                    ActiveSubstance = table.Column<string>(nullable: true),
+                    Weight = table.Column<decimal>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    Amount = table.Column<int>(nullable: false),
+                    ExpirationDate = table.Column<DateTime>(nullable: false),
+                    PrescriptionId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedicineEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MedicineEntity_Prescriptions_PrescriptionId",
+                        column: x => x.PrescriptionId,
+                        principalTable: "Prescriptions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MedicineEntity_PrescriptionId",
+                table: "MedicineEntity",
+                column: "PrescriptionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prescriptions_DoctorId",
+                table: "Prescriptions",
+                column: "DoctorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MedicineEntity");
+
+            migrationBuilder.DropTable(
+                name: "Prescriptions");
+
+            migrationBuilder.DropTable(
+                name: "DoctorEntity");
+        }
+    }
+}
---
+++ LekMed.Database/Migrations/20210130181016_ChangedTypeOfMedicineDbSet.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LekMed.Database.Migrations
+{
+    public partial class ChangedTypeOfMedicineDbSet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MedicineEntity_Prescriptions_PrescriptionId",
+                table: "MedicineEntity");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Prescriptions_DoctorEntity_DoctorId",
+                table: "Prescriptions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MedicineEntity",
+                table: "MedicineEntity");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DoctorEntity",
+                table: "DoctorEntity");
+
+            migrationBuilder.RenameTable(
+                name: "MedicineEntity",
+                newName: "Medicines");
+
+            migrationBuilder.RenameTable(
+                name: "DoctorEntity",
+                newName: "Doctors");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_MedicineEntity_PrescriptionId",
+                table: "Medicines",
+                newName: "IX_Medicines_PrescriptionId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Medicines",
+                table: "Medicines",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Doctors",
+                table: "Doctors",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Medicines_Prescriptions_PrescriptionId",
+                table: "Medicines",
+                column: "PrescriptionId",
+                principalTable: "Prescriptions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Prescriptions_Doctors_DoctorId",
+                table: "Prescriptions",
+                column: "DoctorId",
+                principalTable: "Doctors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Medicines_Prescriptions_PrescriptionId",
+                table: "Medicines");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Prescriptions_Doctors_DoctorId",
+                table: "Prescriptions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Medicines",
+                table: "Medicines");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Doctors",
+                table: "Doctors");
+
+            migrationBuilder.RenameTable(
+                name: "Medicines",
+                newName: "MedicineEntity");
+
+            migrationBuilder.RenameTable(
+                name: "Doctors",
+                newName: "DoctorEntity");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Medicines_PrescriptionId",
+                table: "MedicineEntity",
+                newName: "IX_MedicineEntity_PrescriptionId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MedicineEntity",
+                table: "MedicineEntity",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DoctorEntity",
+                table: "DoctorEntity",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MedicineEntity_Prescriptions_PrescriptionId",
+                table: "MedicineEntity",
+                column: "PrescriptionId",
+                principalTable: "Prescriptions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Prescriptions_DoctorEntity_DoctorId",
+                table: "Prescriptions",
+                column: "DoctorId",
+                principalTable: "DoctorEntity",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
