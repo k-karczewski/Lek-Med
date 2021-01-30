5593fa95442e15a1aa46a88140a14426782472af
Konrad Karczewski <kkarczewski94@gmail.com>
2021-01-30 18:37:59 +0100

    First version od DbContext

---
+++ LekMed.Database/DbContext/LekMedDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LekMed.Database
+{
+    public class LekMedDbContext : DbContext
+    {
+        public LekMedDbContext(DbContextOptions options): base(options) { }
+    }
+}
