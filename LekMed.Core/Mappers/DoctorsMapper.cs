c16899f715a958fcc7f4b480018a30043c0e24eb
Konrad Karczewski <kkarczewski94@gmail.com>
2021-01-31 10:30:09 +0100

    Added regions

--- LekMed.Core/Mappers/DoctorsMapper.cs
+++ LekMed.Core/Mappers/DoctorsMapper.cs
@@ -20,6 +20,7 @@ namespace LekMed.Core
             }).CreateMapper();
         }
 
+        #region Doctor Maps
         // doctor entity => doctor dto
         public DoctorDto Map(DoctorEntity doctorEntity) => _mapper.Map<DoctorDto>(doctorEntity);
         public IEnumerable<DoctorDto> Map(IEnumerable<DoctorEntity> doctorEntities) => _mapper.Map<IEnumerable<DoctorDto>>(doctorEntities);
@@ -28,6 +29,9 @@ namespace LekMed.Core
         public DoctorEntity Map(DoctorDto doctorDto) => _mapper.Map<DoctorEntity>(doctorDto);
         public IEnumerable<DoctorEntity> Map(IEnumerable<DoctorDto> doctorDtos) => _mapper.Map<IEnumerable<DoctorEntity>>(doctorDtos);
 
+        #endregion
+
+        #region Prescription Maps
         // prescription entity => prescription dto
         public PrescriptionDto Map(PrescriptionEntity prescriptionEntity) => _mapper.Map<PrescriptionDto>(prescriptionEntity);
         public IEnumerable<PrescriptionDto> Map(IEnumerable<PrescriptionEntity> prescriptionEntities) => _mapper.Map<IEnumerable<PrescriptionDto>>(prescriptionEntities);
@@ -35,7 +39,9 @@ namespace LekMed.Core
         // prescription dto => prescription entity
         public PrescriptionEntity Map(PrescriptionDto prescriptionDto) => _mapper.Map<PrescriptionEntity>(prescriptionDto);
         public IEnumerable<PrescriptionEntity> Map(IEnumerable<PrescriptionDto> prescriptionDtos) => _mapper.Map<IEnumerable<PrescriptionEntity>>(prescriptionDtos);
+        #endregion
 
+        #region Medicine Maps
         // medicine entity => medicine dto
         public MedicineDto Map(MedicineEntity medicineEntity) => _mapper.Map<MedicineDto>(medicineEntity);
         public IEnumerable<MedicineDto> Map(IEnumerable<MedicineEntity> medicineEntities) => _mapper.Map<IEnumerable<MedicineDto>>(medicineEntities);
@@ -43,5 +49,6 @@ namespace LekMed.Core
         // medicine dto => medicine entity
         public MedicineEntity Map(MedicineDto medicineDto) => _mapper.Map<MedicineEntity>(medicineDto);
         public IEnumerable<MedicineEntity> Map(IEnumerable<MedicineDto> medicineDtos) => _mapper.Map<IEnumerable<MedicineEntity>>(medicineDtos);
+        #endregion
     }
 }
